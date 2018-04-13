9e340ff290d1c800380d431a6a06f10a11083ba0
Daniel Mack <daniel.mack@powerhrg.com>
2018-04-13 23:53:08 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function theBeatlesPlay(musicians, instruments) {
+  let music = []
+  for (let i = 0; i < instruments.length; i++) {
+  music.push(musicians[i] + " plays " + instruments[i])
+  }
+  return music
+}
+
+function johnLennonFacts(array) {
+  let facts = []
+  let i = 0
+while (i < array.length) {
+ facts.push(array[i] + '!!!')
+ i++
+}
+ return facts
+}
